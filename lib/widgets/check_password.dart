793b5a003b4f34d0d346db7fc34f7b3ac61649a8
eslamsalehtaha <eslam1995saleh@gmail.com>
2022-12-21 15:23:25 +0200

    welcome pg

--- lib/widgets/check_password.dart
+++ lib/widgets/check_password.dart
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-=======
->>>>>>> e547e8e (welcome pg)
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:untitled1/providers/providers.dart';
 import 'package:provider/provider.dart';
